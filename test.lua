45bb30c4fd75107619047faee122a16a2e522fcd
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-18 23:13:38 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,10 +1,24 @@
-_G.skillbypass = true
+_G.skillbypass1 = true
 
 coroutine.wrap(function()
-    while _G.skillbypass do task.wait()
+    while _G.skillbypass1 do task.wait()
         local args = {
             [1] = "FrostPhoenixAttack1",
             [2] = Vector3.new(1346.30322265625, 14533.12890625, -879.076904296875),
+            [3] = Vector3.new(1335.976318359375, 14234.0791015625, -884.5792236328125)
+        }
+        
+        game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))            
+    end
+end)()
+
+_G.skillbypass2 = true
+
+coroutine.wrap(function()
+    while _G.skillbypass2 do task.wait()
+        local args = {
+            [1] = "FrostPhoenixAttack1",
+            [2] = Vector3.new(1335.976318359375, 14234.0791015625, -884.5792236328125),
             [3] = Vector3.new(1335.976318359375, 14234.0791015625, -884.5792236328125)
         }
         
