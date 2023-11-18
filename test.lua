98a93b07ada72a0c63c3eebbb1b5e3d3b8316c5f
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-18 17:05:34 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -9,10 +9,8 @@ while true do
         game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
 end
 
-_G.quest = true
-
-coroutine.wrap(function()
-    while _G.quest do task.wait()
+while true do
+    wait()
 local args = {
     [1] = "GetAscendedQuest",
     [2] = 4
@@ -20,15 +18,5 @@ local args = {
 
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
 end
-end)()
 
-_G.teleport = true
-while _G.teleport do 
-    wait()
-coroutine.wrap(function()
-
-    while true do task.wait()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1345.97998, 14532.5098, -876.276062, 0.881768107, -0.463203996, 0.0890339389, -1.20901511e-08, 0.188757926, 0.982023656, -0.471683145, -0.865917146, 0.16644071)
-end
-end)()
-end
+wait()
