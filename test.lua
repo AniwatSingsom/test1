0c9dc31ab538d3ef2038f94d9e4bb028fc44e747
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-18 16:55:31 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,31 +1,3 @@
-_G.quest = true
-
-coroutine.wrap(function()
-    while _G.quest do task.wait()
-local args = {
-    [1] = "GetAscendedQuest",
-    [2] = 4
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
-end
-end)()
-
-_G.teleport = false
-
-_G.teleport = true
-while _G.teleport do 
-    wait()
-coroutine.wrap(function()
-
-    while true do task.wait()
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1345.97998, 14532.5098, -876.276062, 0.881768107, -0.463203996, 0.0890339389, -1.20901511e-08, 0.188757926, 0.982023656, -0.471683145, -0.865917146, 0.16644071)
-end
-end)()
-end
-
-
-
 _G.skill = true
 
 coroutine.wrap(function()
@@ -41,4 +13,26 @@ coroutine.wrap(function()
 end
 end)()
 
+_G.quest = true
 
+coroutine.wrap(function()
+    while _G.quest do task.wait()
+local args = {
+    [1] = "GetAscendedQuest",
+    [2] = 4
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
+end
+end)()
+
+_G.teleport = true
+while _G.teleport do 
+    wait()
+coroutine.wrap(function()
+
+    while true do task.wait()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(1345.97998, 14532.5098, -876.276062, 0.881768107, -0.463203996, 0.0890339389, -1.20901511e-08, 0.188757926, 0.982023656, -0.471683145, -0.865917146, 0.16644071)
+end
+end)()
+end
