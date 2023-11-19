2f1bd5c54b0cce3f2e2fd0948d37f0d3b7b0148a
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-19 11:00:54 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -44,7 +44,7 @@ game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpa
     end
 end)()
 
-wait(80)
+wait(90)
 
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
 
