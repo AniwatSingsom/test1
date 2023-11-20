881a1f840d8ee58e4a6634594bd8f8fbb440511d
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-20 21:07:38 +0700

    Update test1.lua

--- test1.lua
+++ test1.lua
@@ -1,30 +1,6 @@
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
-
-local Player = game.Players.LocalPlayer
-local NotificationLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Suricato006/Scripts-Made-by-me/master/Libraries/Notification%20Library%20Optimization.lua"))()
-local function RemoteAttack(Number, AttackPosition)
-    if Player.Stats.Class.Value == "Angel" then
-        Player.Stats.Class.Value = "Puri Puri"
-    end
-    if Player.Stats.Class.Value == "Toxin" then
-        Player.Stats.Class.Value = "Broly"
-    end
-    local ClassString = string.gsub(Player.Stats.Class.Value, " ", "")
-    local AttackArg = ClassString.."Attack"..tostring(Number)
-    game:GetService("ReplicatedStorage").RemoteEvent:FireServer(AttackArg, AttackPosition)
-end
-
-if Player.Character:FindFirstChild("Form") and (Player.Character.Form.Value == "") then
-    RemoteAttack(6)
-end
-
-Player.CharacterAdded:Connect(function()
-    task.wait(2)
-    RemoteAttack(6)
-end)        
-NotificationLibrary.CustomNotification("test", "auto boss hop + skill bypass", 9e9)
 _G.skillbypass = true
 
 coroutine.wrap(function()
@@ -65,19 +41,7 @@ local args = {
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
     end
 end)()
-wait()
-repeat wait() until game.CoreGui:FindFirstChild('RobloxPromptGui')
- 
-local lp,po,ts = game:GetService('Players').LocalPlayer,game.CoreGui.RobloxPromptGui.promptOverlay,game:GetService('TeleportService')
- 
-po.ChildAdded:connect(function(a)
-    if a.Name == 'ErrorPrompt' then
-        repeat
-            ts:Teleport(game.PlaceId)
-            wait(2)
-        until false
-    end
-end)
+
 wait(50)
 
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
