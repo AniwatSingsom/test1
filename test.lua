c47ed690959e76cf6fcf8ded17c4a230bc43b710
Kamado Irumishiwa <93217970+xS2xits@users.noreply.github.com>
2023-11-20 10:02:35 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -7,14 +7,14 @@ coroutine.wrap(function()
     while _G.skillbypass do task.wait()
 local args = {
     [1] = "AzureAttack1",
-    [2] = Vector3.new(-5110.25439, 14379.001, 3501.29663, 0.950702667, -7.3621635e-08, 0.310103893, 5.22272323e-08, 1, 7.7293663e-08, -0.310103893, -5.72874228e-08, 0.950702667)
+    [2] = Vector3.new(-1363.71338, 16506.5586, -2215.19434, 0.866114795, -1.20218422e-08, 0.499845117, 2.03063326e-08, 1, -1.11349951e-08, -0.499845117, 1.97942054e-08, 0.866114795)
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
 wait()
 local args = {
     [1] = "AzureAttack5",
-    [2] = Vector3.new(-5110.25439, 14379.001, 3501.29663, 0.950702667, -7.3621635e-08, 0.310103893, 5.22272323e-08, 1, 7.7293663e-08, -0.310103893, -5.72874228e-08, 0.950702667)
+    [2] = Vector3.new(-1363.71338, 16506.5586, -2215.19434, 0.866114795, -1.20218422e-08, 0.499845117, 2.03063326e-08, 1, -1.11349951e-08, -0.499845117, 1.97942054e-08, 0.866114795)
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
@@ -27,14 +27,14 @@ coroutine.wrap(function()
     while _G.Quest do task.wait()
 local args = {
     [1] = "GetAscendedQuest",
-    [2] = 10
+    [2] = 12
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvent"):FireServer(unpack(args))
     end
 end)()
 
-wait(150)
+wait(100)
 
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
 
